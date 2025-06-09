e87fd2c7fbd80327ec99cab6df700f49a5ada502
Lincac <83110022+Lincac@users.noreply.github.com>
2025-06-09 14:43:23 +0800

    feat：工程初始化，添加相关第三方工具

---
+++ build_vs2022_x64_Debug.bat
@@ -0,0 +1,19 @@
+@echo off
+
+echo Creating build directory...
+mkdir build
+cd build
+
+echo Running CMake...
+cmake -G "Visual Studio 17 2022" -A x64 -T v141 -DCMAKE_INSTALL_PREFIX=build -DCMAKE_SYSTEM_VERSION=10.0.20348.0 -DCMAKE_CONFIGURATION_TYPES="Release;Debug" ..
+
+echo Building project...
+@REM msbuild ALL_BUILD.vcxproj /p:Configuration=Release
+cmake --build . --config Debug
+
+echo Installing project...
+@REM msbuild INSTALL.vcxproj /p:Configuration=Release
+
+cmake --install . --config Debug
+
+pause
