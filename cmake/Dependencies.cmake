3b04c433117abe72e6872f7ff23a887ef1b3b97d
Lincac <83110022+Lincac@users.noreply.github.com>
2026-02-28 13:42:21 +0800

    feat：重构

--- cmake/Dependencies.cmake
+++
@@ -1,7 +0,0 @@
-include_guard(GLOBAL)
-
-# Add custom CMake module path (if later you provide FindXXX.cmake files).
-list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
-
-# --- Vulkan SDK ---
-find_package(Vulkan REQUIRED)
--- vkRender/vkHelper.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <vector>
-#include <fstream>
-#include <stdexcept>
-#include <filesystem>
-
-std::vector<uint32_t> readSpvFile(const std::filesystem::path& path);
