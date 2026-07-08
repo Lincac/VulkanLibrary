9f5ba3462d7c5dc0c58c932d817c7c0462a48938
lincian <2860807719@qq.com>
2026-07-08 15:46:24 +0800

    feat：加入SDL窗口

--- matVkEngine/matVkEngine/src/matVkEngineContext.h
+++ matVkEngine/matVkEngine/src/matVkEngineContext.h
@@ -7,45 +7,12 @@
 
 #define MAX_FRAMES_IN_FLIGHT 1
 
-/// <summary> GLFW
-/// uint32_t n = 0;
-/// const char** exts = glfwGetRequiredInstanceExtensions(&n);
-///
-/// VkEngineContextCreateInfo info{};
-/// info.surface.instanceExtensions.assign(exts, exts + n);
-/// info.surface.createSurface = [window](VkInstance inst, VkSurfaceKHR* out) {
-///     return glfwCreateWindowSurface(inst, window, nullptr, out);
-/// };
-///
-/// info.surface.chooseSwapExtent = [window](const VkSurfaceCapabilitiesKHR& caps) {
-///    if (caps.currentExtent.width != UINT32_MAX) return caps.currentExtent;
-///    int w, h;
-///    glfwGetFramebufferSize(window, &w, &h);
-///    return clampExtent({(uint32_t)w, (uint32_t)h}, caps);
-/// };
-/// </summary>
-
-/// <summary> SDL
-/// info.surface.instanceExtensions = { /* SDL_Vulkan_GetInstanceExtensions 返回的列表 */ };
-/// info.surface.createSurface = [window](VkInstance inst, VkSurfaceKHR* out) {
-///     return SDL_Vulkan_CreateSurface(window, inst, nullptr, out);
-/// };
-/// </summary>
-
-/// <summary> Qt
-/// info.surface.instanceExtensions = { /* QVulkanInstance::supportedExtensions() */ };
-/// info.surface.createSurface = [qWindow](VkInstance inst, VkSurfaceKHR* out) {
-///     return QVulkanInstance::surfaceForWindow(qWindow, out);  // 或平台等价 API
-/// };
-/// </summary>
-
 namespace mat {
 
     struct VkEngineSurface {
         std::vector<const char*> instanceExtensions;
 
         std::function<VkResult(VkInstance instance, VkSurfaceKHR* outSurface)> createSurface;
-        std::function<void(VkInstance instance, VkSurfaceKHR surface)> destroySurface;
         std::function<VkExtent2D()> queryFramebufferExtent;
     };
 
--- matVkEngine/matVkEngineTriangle/main.cpp
+++ matVkEngine/matVkEngineTriangle/main.cpp
@@ -1,6 +1,44 @@
-﻿#include "matVkEngineContext.h"
+﻿#include <SDL3/SDL.h>
+#include <SDL3/SDL_vulkan.h>
 
-int main()
-{
-    mat::VkEngineContext context;
+#include "matVkEngineContext.h"
+
+int main() {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow("matVkEngineTriangle", 1280, 720, SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE);
+    Uint32 extCount = 0;
+
+    const char* const* sdlExtensions = SDL_Vulkan_GetInstanceExtensions(&extCount);
+
+    mat::VkEngineSurface surf;
+    surf.instanceExtensions.assign(sdlExtensions, sdlExtensions + extCount);
+    surf.createSurface = [window](VkInstance inst, VkSurfaceKHR* out) -> VkResult {
+        if (!SDL_Vulkan_CreateSurface(window, inst, nullptr, out)) {
+            std::fprintf(stderr, "SDL_Vulkan_CreateSurface: %s\n", SDL_GetError());
+            return VK_ERROR_INITIALIZATION_FAILED;
+        }
+        return VK_SUCCESS;
+    };
+    surf.queryFramebufferExtent = [window]() -> VkExtent2D {
+        int w = 0, h = 0;
+        SDL_GetWindowSizeInPixels(window, &w, &h);
+
+        return VkExtent2D{static_cast<uint32_t>(w), static_cast<uint32_t>(h)};
+    };
+
+    mat::VkEngineContext context(surf);
+
+    while (true) {
+        SDL_Event event;
+        SDL_PollEvent(&event);
+
+        if (event.type == SDL_EVENT_QUIT) {
+            break;
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 }
